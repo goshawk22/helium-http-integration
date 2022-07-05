eb4f3436796c78791e13f1909aacc91f278b35c1
Adam Lawson <dev@goshawk22.uk>
2022-07-05 10:19:33 +0100

    Separate packets by date as well as device

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ if not os.path.exists('packets'):
 # Import libraries
 from fastapi import FastAPI, Request
 import json
-import datetime
+from datetime import datetime, date
 
 # Create FastAPI app
 app = FastAPI()
@@ -15,15 +15,21 @@ app = FastAPI()
 @app.post("/")
 async def root(info : Request):
     req_info = await info.json()
+    date = date.now()
+    time = datetime.now()
+
+    date_path = 'packets/' + date + '/'
+    if not os.path.exists(date_path):
+        os.makedirs(date_path)
 
     dev_name = req_info['name']
+
     # Save packet using current time
-    time = datetime.datetime.now()
-    folder = 'packets/' + str(dev_name)
+    folder = date_path + str(dev_name)
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    path = 'packets/' + str(dev_name) + '/' + str(time) + '.json'
+    path = date_path + str(dev_name) + '/' + str(time) + '.json'
     with open(path, 'w+') as f:
         json.dump(req_info, f, indent=4)
 
